1d0cd234fc230cc573caec4920339a7e43ef30d8
akshajdevkv <akshajtechtips@gmail.com>
2020-09-14 11:31:20 +0530

    Update shortlinkmaker.py

--- shortlinkmaker.py
+++ shortlinkmaker.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.common import exceptions
-import time
-import logging
 options = Options()
 options.headless = True
-driver = webdriver.Firefox(executable_path= '/home/user/Desktop/PythonProjects/shortlink-maker/geckodriver',options=options)
+driver = webdriver.Firefox(executable_path= 'your/driver/path',options=options)
 class Short_Link_Maker:
 	def __init__(self):
 		None 
