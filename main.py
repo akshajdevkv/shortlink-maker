45bd10aa8accd0bcf937abe9d6e25c9ee5bdef05
akshajdevkv <akshajtechtips@gmail.com>
2020-09-14 11:28:20 +0530

    update

---
+++ main.py
@@ -0,0 +1,6 @@
+import shortlinkmaker
+
+url = 'https://github.com/akshajdevkv/'
+
+my_link = shortlinkmaker.Short_Link_Maker()
+print(my_link.shorten(url))
--- shortlinkmaker.py
+++ shortlinkmaker.py
@@ -1,24 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.common import exceptions
 import time
-import sys
-driver = webdriver.Firefox(executable_path= './geckodriver')
+import logging
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(executable_path= '/home/user/Desktop/PythonProjects/shortlink-maker/geckodriver',options=options)
+class Short_Link_Maker:
+	def __init__(self):
+		None 
+	def shorten(self,url):
+		self.url = url
+		try:
+			link = self.url
+			driver.get('https://bitly.com/')
+			driver.implicitly_wait(10)
+			url_input = driver.find_element_by_id('shorten_url')
+			url_input.send_keys(link)
+			driver.find_element_by_id('shorten_btn').click()
+			driver.implicitly_wait(10)
+			shortlink = driver.find_element_by_css_selector('.short-link > a:nth-child(1)').text
+			print('Original Link = '+link)
+			print('Shortened Link = '+shortlink+'\n')
 
-try:
-	links = sys.argv[1:]
+		except Exception as e:
+			print(e)
 
-	for link in links:
-		driver.get('https://bitly.com/')
-		url_input = driver.find_element_by_id('shorten_url')
-		url_input.send_keys(link)
-		driver.find_element_by_id('shorten_btn').click()
-		time.sleep(3)
-		#stores the shortened URL 
-		shortlink = driver.find_element_by_css_selector('.short-link > a:nth-child(1)').text
-		print('Original Link = '+link)
-		print('Shortened Link = '+shortlink+'\n')
-	
-
-except Exception as e:
-	print(e)
-	#closes the browser
-	driver.quit()
+		driver.quit()
